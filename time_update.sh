79b657b8aaf0e3e7bce837153161d158f4eee340
shriarul5273 <shriarulmozhivarman643@gmail.com>
2026-06-26 21:25:31 +0900

    initial commit

---
+++ time_update.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+UTC_TIME=$(date -u "+%A, %Y-%m-%d %H:%M:%S UTC")
+LOCAL_TIME=$(date "+%A, %Y-%m-%d %H:%M:%S %Z")
+
+if grep -q "^The day and time is:" README.md; then
+    sed -i "s/^The day and time is:.*$/The day and time is: \"${UTC_TIME}\"/" README.md
+else
+    echo "The day and time is: \"${UTC_TIME}\"" >> README.md
+fi
+
+if grep -q "^The local day and time is:" README.md; then
+    sed -i "s/^The local day and time is:.*$/The local day and time is: \"${LOCAL_TIME}\"/" README.md
+else
+    printf "\nThe local day and time is: \"${LOCAL_TIME}\"\n" >> README.md
+fi
